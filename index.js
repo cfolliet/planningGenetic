f4e1c73f679c37b871cdd21500eab65b90550671
cfolliet <cfolliet@talentsoft.com>
2020-11-26 20:12:53 +0100

    add mutation

--- index.js
+++ index.js
@@ -46,32 +46,74 @@ function mustWorkOncePerDay(calendar) {
     })
 }
 
-const MAX_POP = 500;
+function copy(calendar) {
+    const copy = getCalendar();
 
-function getPopulation(old = []) {
-    let pop = [];
+    calendar.forEach((day, index) => {
+        for (let slot in day) {
+            copy[index][slot] = day[slot];
+        }
+    })
 
-    // keep best score
-    pop = old.sort((a, b) => b.score - a.score).slice(0, 10);
+    return copy;
+}
 
-    //complete with random
-    for (let index = pop.length - 1; index < MAX_POP; index++) {
-        pop.push(getRandomCalendar());
+function mutate(calendar) {
+    const MUTATION_RATE = 0.1;
+    calendar.forEach(day => {
+        for (let slot in day) {
+            if (Math.random() < MUTATION_RATE) {
+                day[slot] = Math.floor(Math.random() * 10);
+            }
+        }
+    })
+    return calendar;
+}
+
+function getSubPopulation(population, nb) {
+    const pop = [];
+
+    for (let i = 0; i < nb; i++) {
+        pop.push(population[Math.floor(Math.random() * population.length)])
     }
 
     return pop;
 }
 
+const MAX_POP = 500;
+
+function getPopulation(old = []) {
+    let population = [];
+
+    // sort by score
+    old.sort((a, b) => b.score - a.score);
+
+    // keep bests score
+    population = old.slice(0, Math.floor(old.length / 3));
+
+    // mutate some
+    if (population.length) {
+        population = population.concat(old.slice(0, Math.floor(population.length / 3)).map(calendar => mutate(copy(calendar))));
+    }
+
+    //complete with random
+    for (let index = population.length - 1; index < MAX_POP; index++) {
+        population.push(getRandomCalendar());
+    }
+
+    return population;
+}
+
 function run() {
-    const maxRun = 1000;
+    const maxRun = 500;
     let nbRun = 0;
 
-    let population = getPopulation([]);
+    let population = [];
 
     while (nbRun < maxRun) {
         nbRun++;
-        population.forEach(calendar => evaluate(calendar));
         population = getPopulation(population);
+        population.forEach(calendar => evaluate(calendar));
         console.log(nbRun + '/' + maxRun)
     }
 
